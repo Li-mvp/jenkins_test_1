d528542dbb6366911f242c359769cf1252c7a981
Renato <1625107364@qq.com>
2021-03-06 13:42:51 +0800

    提交Jenkins项目

---
+++ scripts/test_1.py
@@ -0,0 +1,11 @@
+
+class Test1:
+    def test_1(self):
+        print("test1")
+        assert False
+    def test_2(self):
+        print("test2")
+        assert True
+    def test_3(self):
+        print("test3")
+        assert True
